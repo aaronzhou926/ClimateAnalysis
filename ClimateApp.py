dbef42b7b6096bb613ca360b90d3a926f1a69678
Aaron Zhou <aaronzhou@Aarons-MacBook-Pro.local>
2019-02-21 20:20:58 -0800

    update

--- ClimateApp.py
+++ ClimateApp.py
@@ -22,6 +22,7 @@ Base.prepare(engine, reflect=True)
 
 # Save reference to the table
 Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -31,33 +32,95 @@ session = Session(engine)
 #################################################
 app = Flask(__name__)
 
-
 #################################################
 # Flask Routes
 #################################################
 
 @app.route("/")
-def welcome():
+def index():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
+        f" - list of rain totals from all stations<br/>"
         f"/api/v1.0/stations<br/>"
+        f"- list of Station numbers and names<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start>/<br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"- list of prior year temperatures from all stations<br/>"
+        f"/api/v1.0/STARTTIME<br/>"
+        f"- When given the start date (yyyy-mm-dd), calculates the min/ave/max temp for all dates greate than and equal to teh start date.<br/>"
+        f"/api/v1.0/STARTTIME/ENDTIME<br/>"
+        f"- Please provide the start date (yyyy-mm-dd) and end date (yyyy-mm-dd)"
         )
 
-@app.route("/api/v1.0/percipitation")
-def names():
+@app.route("/api/v1.0/precipitation")
+def percipitation():
     """Return percipitation"""
-    # Query all passengers
-    results = (session.query(Measurement.date, Measurement.prcp)
-                   .filter((Measurement.date))
-                   .group_by(Measurement.date)
+    # Query all date and prcp from Measurement
+    results = session.query(Measurement).all()
+    # Create a dictionary from the row data and append to a list of all_stations
+    all_stations = []
+
+    for result in results:
+        stations_dict = {}
+        stations_dict["date"] = result.date
+        stations_dict["prcp"] = result.prcp
+        all_stations.append(stations_dict)
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return Station Names"""
+    results = session.query(Station.station, Station.name,).all()
+    # Create a dictionary from the row data and append to a list of all_stations
+    all_stations=[]
+    for result in results:
+        stations_dict = {}
+        stations_dict["station"] = result[0]
+        stations_dict["name"] = result[1]
+        all_stations.append(stations_dict)
+    
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    #query for the dates and temps froma year fromt the last data point.
+    query_date = dt.date(2017,8,23) - dt.timedelta(weeks=52)
+    #create all data from 2016/8/23-2017/8/23
+    lasttwelvemonths = (session.query(Measurement)
+                   .filter(func.strftime(Measurement.date)>=query_date)
+                   .order_by(Measurement.date)
                    .all())
+    one_year_tobs=[]
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    for last in lasttwelvemonths:
+        tobs_dict = {}
+        tobs_dict["date"] = last.date
+        tobs_dict["tobs"] = last.tobs
+        one_year_tobs.append(tobs_dict)
 
-    return jsonify(all_names)
+    return jsonify(one_year_tobs)
+
+@app.route("/api/v1.0/<start>")
+def trip1(start):
+
+    trip_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),
+    func.max(Measurement.tobs)).filter(Measurement.date>=start).all()
+
+    trip = list(np.ravel(trip_data))
+
+    return jsonify(trip)
+
+@app.route("/api/v1.0/<start>/<end>")
+def trip2(start,end):
+
+    trip_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),
+    func.max(Measurement.tobs)).filter(Measurement.date>=start).filter(Measurement.date<=end).all()
+
+    trip = list(np.ravel(trip_data))
+
+    return jsonify(trip)
+
+if __name__ == '__main__':
+    app.run(debug=True)
